792912b96af1d59f3bd3e63e84e0723a5bffb10a
Roshan Paturkar <paturkarr@gmail.com>
2018-05-30 19:32:33 +0530

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -44,7 +45,7 @@ app.get('/todos/:id', (request, response) => {
     if (!todo) {
       return response.status(404).send()
     }
-    
+
     response.send({todo})
   }).catch((error) => {
     response.status(404).send();
@@ -66,6 +67,32 @@ app.delete('/todos/:id', (request, response) => {
     response.send({todo})
   }).catch((error) => {
     response.status(404).send();
+  });
+});
+
+app.patch('/todos/:id', (request, response) => {
+  var id = request.params.id;
+  var body = _.pick(request.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return response.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return response.status(404).send()
+    }
+
+    response.send({todo});
+  }).catch((error) => {
+    response.status(400).send();
   });
 });
 
